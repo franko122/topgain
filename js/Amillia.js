57e7979003e0c9826a246ebfd01a9de169744f9b
franko122 <83500527+franko122@users.noreply.github.com>
2025-02-06 08:07:24 +0100

    Create Amillia.js

---
+++ js/Amillia.js
@@ -0,0 +1,20 @@
+function starter( ) {
+    let three = document.getElementById("three");
+    
+    three.addEventListener('click' , ()=>{ 
+    let one = document.getElementById("one");
+    let two = document.getElementById("two") ;
+    let wel = document.getElementById("wel");
+    let ede = document.getElementById("ede")
+    let three = document.getElementById("three"); 
+        let Username = "Amillia" 
+        let password = "recoverY21@"   
+        if  ( one.value == Username && two.value == password) {  
+            window.location="dashboardamillia.html"
+        }else{
+            console.log('wrong'); 
+            alert('wrong password')
+        } 
+    })
+}
+starter()￼Enter
